1e3b271995d925dd9bfc421df829830880855fa7
Osman Kızılkaya <145294697+Osmkzlky@users.noreply.github.com>
2025-03-05 23:02:49 +0300

    Update global.dart

--- lib/helpers/global.dart
+++ lib/helpers/global.dart
@@ -1,2 +1,3 @@
-const apiKey =
-    "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";
+// api key
+const apiKey = ""
+   
